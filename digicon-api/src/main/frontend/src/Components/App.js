7188e2cbbc4c94bd0aac13dc78d59fe12924817f
Chris Stubbs <stubbs.chris2@gmail.com>
2020-05-30 22:54:17 -0400

    Improved front end using React

---
+++ digicon-api/src/main/frontend/src/Components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import UserHomePage from "./User Home Page/UserHomePage"
+import "./styles/userhomepage.css"
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <UserHomePage />
+      </div>
+    );
+  }
+}
+
+export default App;
